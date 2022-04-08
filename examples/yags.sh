06192b45749732f4ce9967061e8743375cb8f6a9
Vincent Esposito <espov@slac.stanford.edu>
2022-04-08 11:34:10 -0700

    sizing and cosmetics

--- examples/yags.sh
+++ examples/yags.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 source pcds_conda
+export EPICS_HOST_ARCH="rhel7-x86_64" # fix for env var overwritten by pcds_conda
 
 export MYDIR=`dirname $(readlink -f $0)` # gets script working dir
 
