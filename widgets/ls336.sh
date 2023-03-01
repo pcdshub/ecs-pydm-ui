998542ef0e8dcc29308ec97952ac6775a170f0e9
Vincent Esposito <espov@slac.stanford.edu>
2023-02-28 17:50:06 -0800

    remove hardcoded controller

--- widgets/ls336.sh
+++ widgets/ls336.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 cd /cds/group/pcds/epics/screens/edm/xpp/R0.3.0
-edm -eolc -x -m "DEV=XPP:XCS:TCT:01" ls336Screens/lakeshore336.edl
-#edm -eolc -x -m "DEV=$1" ls336Screens/lakeshore336.edl
+edm -eolc -x -m "DEV=$1" ls336Screens/lakeshore336.edl
