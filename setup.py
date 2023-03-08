6fee7431f49c18f895f7c0f1a0b2a1432bbac9e8
Vincent Esposito <espov@slac.stanford.edu>
2023-03-08 15:29:53 -0800

    update setup.py before migration

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ if len(git_requirements) > 0:
     print("User must install the following packages manually:\n"
           "\n".join(f' {r}' for r in git_requirements))
 
-setup(name='lcls1_screens',
+setup(name='lcls_screens',
       author='ECS',
       packages=find_packages(),
       include_package_data=True,
       install_requires=requirements,
-      description='Custom set of pydm screens for LCLS-I hutches',
+      description='Custom set of pydm screens for LCLS hutches',
       )
